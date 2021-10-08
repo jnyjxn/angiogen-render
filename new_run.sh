b67741a17375064ba24d47347e8116877ce5d6ce
Jonny <jonny@jxn.co>
2021-10-08 22:30:17 +0100

    Renamed run scripts.

--- new_run.sh
+++
@@ -1,3 +0,0 @@
-CONTAINER_NAME=flask
-sudo docker build -t $CONTAINER_NAME .
-sudo docker run --rm -p 5000:5000 -v /Users/jonnyjackson/Documents/PhD:/data -it $CONTAINER_NAME:latest
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 CONTAINER_NAME=flask
 sudo docker build -t $CONTAINER_NAME .
-xhost +local:`sudo docker inspect --format='{{ .ContainerConfig.Hostname }}' $CONTAINER_NAME`
-sudo nvidia-docker run --rm --gpus all -p 5000:5000 -v /data4/jja17/datasets/sparse_geometry:/data -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -it $CONTAINER_NAME:latest
+sudo docker run --rm -p 5000:5000 -v /data4/jja17/datasets/sparse_geometry:/data -it $CONTAINER_NAME:latest
