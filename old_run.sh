f1e72d49cdb30351726efeb069cefb75032a0c5d
Jonny <jonny@jxn.co>
2021-11-18 14:58:45 +0000

    Cleaned up unused files

--- old_run.sh
+++
@@ -1,4 +0,0 @@
-CONTAINER_NAME=flask
-sudo docker build -t $CONTAINER_NAME .
-xhost +local:`sudo docker inspect --format='{{ .ContainerConfig.Hostname }}' $CONTAINER_NAME`
-sudo nvidia-docker run --rm --gpus all -p 5000:5000 -v /data4/jja17/datasets/sparse_geometry:/data -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -it $CONTAINER_NAME:latest
